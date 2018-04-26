62727a5c73b6461763554572adbbdd7f72f7b0da
STEFAN <hotpea@gmail.com>
2018-04-26 16:22:40 -0300

    * fullpage.js * reorganizing page to use fullpage.js and shuffle.js

--- js/script.js
+++ js/script.js
@@ -1,249 +1,20 @@
-var top_site = [
-    {
-        id: '.top-site-text',
-        delayBefore: 100,
-        animations: {
-            top: '40%',
-            opacity: 1
-        }
-    }
-];
-
-var trio_one = [
-    {
-        id: '#square_trio_first',
-        animations: {marginTop: 0}
-    },
-    [
-        {
-            id: '#square_first_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_first_second',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_first_third',
-            delayBefore: 100,
-            animations: {
-                right: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_second_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_second_second',
-            delayBefore: 100,
-            animations: {
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_second_third',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_second_four',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                right: '0px',
-                opacity: 1
-            }
-        }
-    ]
-];
-
-var acreditar_animation = [
-    {
-        id: '.acreditar',
-        delayBefore: 100,
-        animations: {
-            'max-width': '30%',
-            opacity: 1
-        }
-    }
-];
-
-var trio_two = [
-    {
-        id: '#square_trio_third',
-        animations: {marginTop: 0}
-    },
-    [
-        {
-            id: '#square_four_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_four_second',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_four_third',
-            delayBefore: 100,
-            animations: {
-                right: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_third_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_third_second',
-            delayBefore: 100,
-            animations: {
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_third_third',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_third_four',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                right: '0px',
-                opacity: 1
-            }
-        }
-    ]
-];
-
-var anos_animation = [
-    {
-        id: '.anos',
-        delayBefore: 100,
-        animations: {
-            'max-width': '30%',
-            opacity: 1
-        }
-    }
-];
-
-var trio_three = [
-    {
-        id: '#square_trio_five',
-        animations: {marginTop: 0}
-    },
-    [
-        {
-            id: '#square_five_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_five_second',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_five_third',
-            delayBefore: 100,
-            animations: {
-                right: '0px',
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_six_first',
-            delayBefore: 100,
-            animations: {
-                left: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_six_second',
-            delayBefore: 100,
-            animations: {
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_six_third',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                opacity: 1
-            }
-        },
-        {
-            id: '#square_six_four',
-            delayBefore: 100,
-            animations: {
-                top: '0px',
-                right: '0px',
-                opacity: 1
-            }
-        }
-    ]
-];
-
-// inicia animações
 var Shuffle = window.Shuffle;
 
-var el = document.querySelector('#shuffle-container');
-//var sizer = document.querySelector('.my-sizer-element');
-var shuffleInstance = new Shuffle(el,{
-    itemSelector: '.picture-item',
-    //sizer: sizer,
-});
+var shuffleDivs = document.getElementsByClassName('shuffle-container')
+var shuffleInstance = [];
 
-var anos = document.getElementById("anos");
-var acreditar = document.getElementById("acreditar");
+for (var i = 0; i < shuffleDivs.length; i++) {
+    var el = document.querySelector("#" + shuffleDivs[i].id);
+
+    //var sizer = document.querySelector('.my-sizer-element');
+    shuffleInstance[i] = new Shuffle(el,{
+        itemSelector: '.picture-item',
+        //sizer: sizer,
+    });
+}
+
+
+
 var todos_shuffle = document.getElementById("todos_shuffle");
 var acreditar_shuffle = document.getElementById("acreditar_shuffle");
 var dois_anos_shuffle = document.getElementById("dois_anos_shuffle");
@@ -253,35 +24,22 @@ var last_shuffle = Shuffle.ALL_ITEMS;
 var original_width = '33%';
 var clicked_width = '100%';
 
-new YAnimation(top_site, {loop: false, clearAfterEnd: false}).startAnimation();
-
-//window.addEventListener('scroll',function(e) {
-//    if(checkvisible(acreditar) && (!valida_one)) {
-//        new YAnimation(trio_one, {loop: false, clearAfterEnd: false}).startAnimation();
-//        valida_one = true;
-//    }
-//    if(checkvisible(anos) && (!valida_two)) {
-//        new YAnimation(trio_two, {loop: false, clearAfterEnd: false}).startAnimation();
-//        new YAnimation(acreditar_animation, {loop: false, clearAfterEnd: false}).startAnimation();
-//        valida_two = true;
-//    }
-//    if(checkvisible(bottom) && (!valida_three)) {
-//        new YAnimation(trio_three, {loop: false, clearAfterEnd: false}).startAnimation();
-//        new YAnimation(anos_animation, {loop: false, clearAfterEnd: false}).startAnimation();
-//        valida_three = true;
-//    }
-//});
-
 // rolar pro topo ao carregar
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
 
 document.body.onclick = function() {
-    shuffleInstance.filter(last_shuffle);
+    for (var i = 0; i < shuffleInstance.length; i++) {
+        shuffleInstance[i].filter(last_shuffle);
+    }
 };
 
 window.onload = function() {
+    var topSite = document.getElementById('top-site-text');
+    topSite.style.opacity = "1";
+    topSite.style.top = "40%";
+
     var controler = document.getElementsByClassName('picture-item');
 
     for (var i = 0; i < controler.length; i++) {
