bdecbe3409453680011635e2df46e4a605da7983
Stéfan de Lima Amaral <hotpea@gmail.com>
2018-05-10 11:31:38 -0300

    Play button refactor

--- js/events.js
+++ js/events.js
@@ -28,23 +28,4 @@ $(document).ready(function() {
             play.parent().append('<iframe class="video-play" frameborder="0" src="https://www.youtube.com/embed/7-tNUur2YoU?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0&autoplay=1" allow="autoplay; encrypted-media"></iframe>');
         }, 400);
     });
-
-    /**
-     * evento de play quando clicado em play do vídeo #2
-     *
-     * TODO: passar para PLAIN JAVASCRIPT
-     */
-    $('.play-button2').click(function() {
-        var play = $(this);
-
-        $('html, body').animate({
-            scrollTop: play.parent().offset().top
-        }, 300);
-
-        play.fadeOut();
-
-        setTimeout(function() {
-            play.parent().append('<iframe class="video-play" frameborder="0" src="https://www.youtube.com/embed/HyHNuVaZJ-k?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=0&autoplay=1" allow="autoplay; encrypted-media"></iframe>');
-        }, 400);
-    });
 });
